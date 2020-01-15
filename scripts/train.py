5e09da4355649a11e877fbdba31fa8addc57e094
gatheluck <gatheluck+tech@gmail.com>
2020-01-16 03:17:07 +0900

    add evaluation to training

--- scripts/train.py
+++ scripts/train.py
@@ -20,6 +20,7 @@ from selectivenet.vgg_variant import vgg16_variant
 from selectivenet.model import SelectiveNet
 from selectivenet.loss import SelectiveLoss
 from selectivenet.data import DatasetBuilder
+from selectivenet.evaluator import Evaluator
 
 # options
 @click.command()
@@ -139,6 +140,10 @@ def train(**kwargs):
                 # total loss
                 loss = selective_loss + ce_loss
                 loss_dict['loss'] = loss.detach().cpu().item()
+
+                # evaluation
+                evaluator = Evaluator(out_class.detach(), t.detach(), out_select.detach())
+                loss_dict.update(evaluator())
 
                 val_metric_dict.update(loss_dict)
 
